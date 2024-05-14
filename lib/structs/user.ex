6ad5a37173e8dcd22e94b5bd0bcbc4a7709cd89f
Yigit Sadic <yigitsadic@gmail.com>
2024-05-14 13:46:44 +0300

    added user struct and tests

---
+++ lib/structs/user.ex
@@ -0,0 +1,62 @@
+defmodule Structs.User do
+  @max_event_queue 10
+  @enforce_keys :username
+  defstruct username: "", currencies: %{}, event_queue: 0
+
+  @type currency_map :: %{String.t() => number()}
+
+  @type t :: %__MODULE__{
+          username: String.t(),
+          currencies: currency_map(),
+          event_queue: number()
+        }
+
+  @doc "Gets requested currency from user."
+  @spec get_currency(user :: t(), currency :: String.t()) :: number()
+  def get_currency(user, currency) when is_map(user) and is_bitstring(currency) do
+    Map.get(user.currencies, currency) || 0
+  end
+
+  @doc "Adds given amount in currency to requested user."
+  @spec add_currency(user :: t(), amount :: number(), currency :: String.t()) :: t()
+  def add_currency(user, amount, currency)
+      when is_map(user) and is_number(amount) and is_bitstring(currency) do
+    new_amount = get_currency(user, currency) + amount
+
+    currencies = user.currencies
+    updated_currencies = Map.put(currencies, currency, new_amount)
+
+    new_user = Map.put(user, :currencies, updated_currencies)
+
+    new_user
+  end
+
+  @doc "Checks that given user can withdraw amount in currency."
+  @spec can_withdraw?(user :: t(), amount :: number(), currency :: String.t()) :: boolean()
+  def can_withdraw?(user, amount, currency) do
+    current_amount = get_currency(user, currency)
+
+    current_amount >= amount
+  end
+
+  @doc "Checks if given users event queue is full."
+  @spec queue_full?(user :: t()) :: {:ok, boolean()} | {:error}
+  def queue_full?(user) when is_map(user) do
+    if :event_queue in Map.keys(user) do
+      {:ok, Map.get(user, :event_queue, 0) >= @max_event_queue}
+    else
+      {:error}
+    end
+  end
+
+  @spec queue_full?(_user :: any()) :: {:error}
+  def queue_full?(_user), do: {:error}
+
+  @doc "Returns a struct filled with given name"
+  @spec new(name :: String.t()) :: t()
+  def new(name) when is_bitstring(name) do
+    %__MODULE__{
+      username: name
+    }
+  end
+end
---
+++ test/structs/user_test.exs
@@ -0,0 +1,52 @@
+defmodule Structs.UserTest do
+  use ExUnit.Case
+  alias Structs.User
+
+  test "can retrieve a currency value" do
+    user1 = User.new("user1")
+
+    assert User.get_currency(user1, "USD") == 0
+
+    user1 = Map.put(user1, :currencies, %{"USD" => 15.75})
+    assert User.get_currency(user1, "USD") == 15.75
+  end
+
+  test "should add currency" do
+    user = User.new("user")
+    user2 = User.new("user2")
+
+    user = User.add_currency(user, 15.75, "USD")
+    user = User.add_currency(user, 27, "EUR")
+    user = User.add_currency(user, 10, "USD")
+
+    assert User.get_currency(user, "USD") == 25.75
+    assert User.get_currency(user, "EUR") == 27
+
+    assert User.get_currency(user2, "USD") == 0
+  end
+
+  test "can check given user withdraw currency" do
+    user = User.new("user")
+
+    user = User.add_currency(user, 15.75, "USD")
+
+    assert User.can_withdraw?(user, 15.76, "USD") == false
+    assert User.can_withdraw?(user, 15.75, "USD") == true
+    assert User.can_withdraw?(user, 15.75, "EUR") == false
+    assert User.can_withdraw?(user, 14.75, "USD") == true
+  end
+
+  test "it should return queue full if it is" do
+    user = User.new("user")
+    user = Map.put(user, :event_queue, 11)
+
+    assert User.queue_full?(user) == {:ok, true}
+    assert User.queue_full?(5) == {:error}
+    assert User.queue_full?("5") == {:error}
+    assert User.queue_full?(nil) == {:error}
+    assert User.queue_full?(%{}) == {:error}
+
+    user = Map.put(user, :event_queue, 9)
+    assert User.queue_full?(user) == {:ok, false}
+  end
+end
