2bd6a489fbe7dff5f2bdc1c3aca47c53053d945d
Yigit Sadic <yigitsadic@gmail.com>
2024-05-14 14:26:36 +0300

    Added scaffold for core functions

--- lib/ex_banking.ex
+++ lib/ex_banking.ex
@@ -1,5 +1,21 @@
 defmodule ExBanking do
-  def hello do
-    :world
+  def create_user(_given) do
+    :ok
+  end
+
+  def deposit(_user, amount, _currency) do
+    {:ok, amount}
+  end
+
+  def withdraw(_user, amount, _currency) do
+    {:ok, amount}
+  end
+
+  def get_balance(_user, _currency) do
+    {:ok, 15}
+  end
+
+  def send(_from_user, _to_user, amount, _currency) do
+    {:ok, 0, amount}
   end
 end
--- test/ex_banking_test.exs
+++ test/ex_banking_test.exs
@@ -1,7 +1,23 @@
 defmodule ExBankingTest do
   use ExUnit.Case
 
-  test "greets the world" do
-    assert ExBanking.hello() == :world
+  test "create_user" do
+    assert ExBanking.create_user("yigit") == :ok
+  end
+
+  test "deposit" do
+    assert ExBanking.deposit("yigit", 15, "EUR") == {:ok, 15}
+  end
+
+  test "withdraw" do
+    assert ExBanking.withdraw("yigit", 15, "EUR") == {:ok, 15}
+  end
+
+  test "get_balance" do
+    assert ExBanking.get_balance("yigit", "EUR") == {:ok, 15}
+  end
+
+  test "send" do
+    assert ExBanking.send("yigit", "sadic", 15, "EUR") == {:ok, 0, 15}
   end
 end
